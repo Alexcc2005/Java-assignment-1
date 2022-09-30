09e27b2b684515a1c68798b17e643048a9d3cb58
Alexcc2005 <112864896+Alexcc2005@users.noreply.github.com>
2022-09-30 11:22:43 +0100

    Add files via upload

---
+++ EX3.js
@@ -0,0 +1,31 @@
+//EXERCISE 3
+
+/*
+Question 1:
+
+Values:
+5
+-88.8
+'hello'
+Operators:
+-
+/
++
+*
+Question 2:
+
+'spam' is a variable and spam is a string.
+Question 3:
+
+number, boolean and string
+Question 4:
+
+21
+Question 5:
+
+spamspamspam
+*/
+//Question 6:
+
+let number1 = Number(prompt("Enter a number between 1-3"))
+let 1 = "Howdy!"
---
+++ Java.js
@@ -0,0 +1,128 @@
+console.log("HelloWorld")
+// Single line Comment
+
+/*
+Multi line comment
+ this will cover
+  multiple lines
+
+*/
+
+//variables
+
+/*
+There are two types of ways to declare variables in JavaScript
+
+var a = 10;
+
+let a = 10;
+
+const x = 10;    This is when you dont want a variable to change
+
+*/
+
+let a =10;
+console.log(a);
+
+const x =20; 
+
+console.log(x)
+
+//x=15
+
+//Variables in JavaScript are case sensitive
+
+let bob=20;
+
+let boB=10;
+
+//Datatypes
+/*
+Numbers
+Strings
+Booleans
+*/
+
+let temp = 10 //Datatype number
+
+let temp1 = "10" //Datatype string
+
+/*
+All numbers in Javascript are floating point numbers
+*/
+
+/*
+All variable names in javascript must begin with a
+letter or an undersore. You cannot use a reserved word
+as a variable name.
+*/
+
+//Arithmetic Operators
+
+//Addition
+
+console.log(5+2);
+
+console.log("abc"+"def");
+
+//Subtraction
+
+console.log(5-2)
+
+//multiplication
+
+console.log(5*2);
+
+//Division
+
+console.log (8/4);
+
+//Exponent
+
+console.log(3**4);
+
+//increment
+
+let h = 11;
+
+console.log(h++);
+
+console.log(h);
+
+let k = 10;
+
+console.log(++k);
+
+//Comparison Oporators
+
+//Equals ==
+
+console.log(5 == 3);
+
+console.log(4 == 4);
+//not equals !=
+
+console.log(5!=4);
+console.log(4!=4);
+
+//Greater than >
+
+console.log(4>5);
+console.log(5>4);
+
+//Less than <
+
+console.log(4<5);
+console.log(5<5);
+
+//Greater than or equal to <=
+
+console.log(4<=5);
+
+console.log(5<=4);
+
+//Less than or equal to >=
+
+console.log(5>=4);
+
+console.log(4>=5);
